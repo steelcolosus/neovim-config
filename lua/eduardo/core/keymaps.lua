20f648875e63707ec26ad67ede89edd813819825
Eduardo Aviles <eduardo.avilesj@gmail.com>
2024-05-14 00:10:00 -0600

    adding shortcut for select all

--- lua/eduardo/core/keymaps.lua
+++ lua/eduardo/core/keymaps.lua
@@ -22,3 +22,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current Tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next Tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- select all text in buffer usinc ctrl + a
+keymap.set("n", "<C-a>", "ggVG", { desc = "Select all text in buffer" })
