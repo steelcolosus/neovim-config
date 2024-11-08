19d586da864e8c29868fc6857dd1bfc6aa84ddda
Eduardo Aviles <eduardo.aviles@loanpro.io>
2024-11-08 13:57:56 -0600

    adding pyright config and vimtext

---
+++ lua/eduardo/plugins/nvim-latex.lua
@@ -0,0 +1,22 @@
+return {
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+        vim.g.vimtex_view_method = "skim"
+        vim.g.vimtex_quickfix_mode = 0
+        vim.g.tex_flavor = "latex"
+
+        vim.g.tex_conceal = "abdmg"
+
+        vim.g.vimtex_compiler_latexmk = {
+            options = {
+                "-pdf",
+                "-shell-escape",
+                "-verbose",
+                "-file-line-error",
+                "-synctex=1",
+                "-interaction=nonstopmode",
+            },
+        }
+    end,
+}
